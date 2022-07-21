59a192b34287f7306d2963b8e7d4ba088e26d014
Jacob Ashkenas <jacobashkenas@gmail.com>
2022-07-21 10:02:50 -0400

    Win detection

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -4,20 +4,42 @@ import "./TicTacToe.css";
 
 function TicTacToe(props) {
     const [currentPlayer, setCurrentPlayer] = useState(Player.Player1);
+    const [winner, setWinner] = useState(Player.None);
     const [board, setBoard] = useState([Player.None, Player.None, Player.None,
                                         Player.None, Player.None, Player.None,
                                         Player.None, Player.None, Player.None]);
 
-    const makeMove = (space) => setBoard(oldBoard => {
-        oldBoard[space] = currentPlayer;
-        setCurrentPlayer(lastPlayer => lastPlayer === Player.Player1 ? Player.Player2 : Player.Player1);
-        return oldBoard;
-    });
+    const makeMove = (space) => {
+        if (board[space] !== Player.None || winner !== Player.None)
+            return;
+
+        board[space] = currentPlayer;
+        setBoard(board);
+        setCurrentPlayer(currentPlayer === Player.Player1 ? Player.Player2 : Player.Player1);
+
+        // Check for winner
+        for (let i = 0; i < 3; i++) {
+            if (board[i * 3] !== Player.None && board[i * 3] === board[i * 3 + 1] && board[i * 3] === board[i * 3 + 2]) {
+                setWinner(board[i * 3]);
+                board[i * 3] = board[i * 3 + 1] = board[i * 3 + 2] = board[i * 3] === Player.Player1 ? Player.Player1Win : Player.Player2Win;
+            } else if (board[i] !== Player.None && board[i] === board[i + 3] && board[i] === board[i + 6]) {
+                setWinner(board[i]);
+                board[i] = board[i + 3] = board[i + 6] = board[i] === Player.Player1 ? Player.Player1Win : Player.Player2Win;
+            }
+        }
+        if (board[4] !== Player.None && board[4] === board[0] && board[4] === board[8]) {
+            setWinner(board[4]);
+            board[0] = board[4] = board[8] = board[4] === Player.Player1 ? Player.Player1Win : Player.Player2Win;
+        } else if (board[4] !== Player.None && board[4] === board[2] && board[4] === board[6]) {
+            setWinner(board[4]);
+            board[2] = board[4] = board[6] = board[4] === Player.Player1 ? Player.Player1Win : Player.Player2Win;
+        }
+    };
 
     return (
         <div className="board">
             {board.map((player, i) =>
-                <button key={i} className={`button ${player.name}`} onClick={player === Player.None ? makeMove.bind(null, i) : ()=>{}}></button>
+                <button key={i} className={`button ${player.name}`} onClick={makeMove.bind(null, i)}></button>
             )}
         </div>
     );
--- src/player.js
+++ src/player.js
@@ -1,7 +1,9 @@
 export default class Player {
     static None = new Player("none");
     static Player1 = new Player("player1");
+    static Player1Win = new Player("player1win");
     static Player2 = new Player("player2");
+    static Player2Win = new Player("player2win");
 
     constructor(name) { this.name = name; }
 };
